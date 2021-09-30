4b54d3d96ba1d0f39b793932246915b2cbaaf8b3
JanmayP <janmay.patel07@gmail.com>
2021-09-30 12:03:06 +0800

    Added functionality to mark subtasks as done

--- src/components/SubTasks.js
+++ src/components/SubTasks.js
@@ -1,11 +1,27 @@
+import { FaTimes } from 'react-icons/fa'
+
 const SubTasks = ({task}) => {
   let cleanSubTasks = task.subTasks.filter((subTask) => subTask !== "")
+
+  const markDone = (e) => {
+    let subTaskDone = e.target.previousSibling
+    if (subTaskDone === null) {
+      subTaskDone = e.target.parentNode.previousSibling
+    }
+    if (subTaskDone.style.textDecorationLine !== "line-through") {
+      subTaskDone.style.textDecorationLine = "line-through"
+    } else {
+      subTaskDone.style.textDecorationLine = "none"
+    }
+  }
+  
   return (
     <div className="task-subtasks">
       {cleanSubTasks.length > 0 
       ? cleanSubTasks.map((subTask) => (
         <div className="task-subtask">
-          {subTask} 
+          <span>{subTask} </span>
+          <FaTimes  className="remove-task" size={18} onClick={(e) => {markDone(e)}} />
         </div>
       )) 
       : <p>Nothing to show!</p>}
